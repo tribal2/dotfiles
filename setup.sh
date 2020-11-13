bf3a09e05450ce0da778c0d432693f99ee3b2678
Ricardo Tribaldos <579671+swordf1zh@users.noreply.github.com>
2020-11-12 21:44:30 -0500

    Install Nerd Font for Alacritty and set font in config

--- setup.sh
+++ setup.sh
@@ -46,7 +46,13 @@ then
   sudo apt-get update -y
   sudo apt-get install -y alacritty
 
+  # Font installation
+  mkdir -p "$HOME"/.local/share/fonts
+  cd "$HOME"/.local/share/fonts
+  curl -fsSLo 'DejaVu Sans Mono Nerd Font.ttf' https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DejaVuSansMono/Regular/complete/DejaVu%20Sans%20Mono%20Nerd%20Font%20Complete.ttf
+  cd
 
+  # Config
   mkdir -p "$CONFDIR"/alacritty
   ln -s "$BASEDIR"/alacritty.yml "$CONFDIR"/alacritty/alacritty.yml
 fi
