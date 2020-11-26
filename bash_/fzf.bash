f2eb9e2c11d630c8a8e803559032c23a4be7c81c
Ricardo Tribaldos <579671+swordf1zh@users.noreply.github.com>
2020-11-26 00:23:56 -0500

    feat: Improved configuration for Fzf

--- bash_/fzf.bash
+++ bash_/fzf.bash
@@ -1,17 +1,37 @@
 export FZF_DEFAULT_OPTS="
+  --preview-window=hidden
   --preview ' \
-    ([[ -d {} ]] && tree -C {}) \
-    || ([[ -f {} ]] && bat --style=full --color=always {}) \
-    || echo {}'
+      ([[ -d {} ]] && tree -aC  -L 2 --dirsfirst {} -I "".git|node_modules"") \
+      || ([[ -f {} ]] && batcat --style=full --color=always {} || cat {}) \
+      || echo {} \
+      2> /dev/null | head -100'
   --height 60%
   --layout=reverse
-  --border"
-export FZF_DEFAULT_COMMAND='fd --type f'
+  --border
+  --multi
+  --bind '?:toggle-preview'
+  --bind 'ctrl-a:select-all'
+  --bind 'ctrl-y:execute-silent(echo {+} | xclip)'
+  --bind 'ctrl-e:execute(echo {+} | xargs -o vim)'
+  --bind 'ctrl-v:execute(code {+} || codium {+})'
+"
+
+export FZF_DEFAULT_COMMAND="fdfind --hidden --follow --exclude '.git' --exclude 'node_modules'"
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
-export FZF_ALT_C_COMMAND="fd -t d . $HOME"
+export FZF_ALT_C_COMMAND="fdfind --type d . $HOME"
 
 source /usr/share/doc/fzf/examples/completion.bash
 source /usr/share/doc/fzf/examples/key-bindings.bash
 
+# ALIASES
 alias fh='history | fzf --tac --no-sort'
-alias fapt='apt-cache pkgnames | fzf -m --preview "cat <(apt-cache show {1} | grep --color -E \"^Package|^Homepage|^Version|^Size|^Description-en|^ \")" --preview-window "80" | xargs -ro sudo apt install'
+alias fpath='echo $PATH | tr ":" "\n" | fzf'
+alias fapt='apt-cache pkgnames \
+  | fzf -m --preview \
+    "cat <(\
+      apt-cache show {1} \
+      | grep --color -E \"^Package|^Homepage|^Version|^Size|^Description-en|^\"\
+    )" \
+    --preview-window "80" \
+  | xargs -ro sudo apt install'
+
