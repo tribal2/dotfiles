69288a10b3749bbf09bc0af519f03de875aee6bc
Ricardo Tribaldos <579671+swordf1zh@users.noreply.github.com>
2020-11-26 00:08:38 -0500

    feat: Add VSCodium installation

--- software/VSCodium.bash
+++ software/VSCodium.bash
@@ -8,12 +8,12 @@ else
     https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg \
     | gpg --dearmor \
     | sudo dd of=/etc/apt/trusted.gpg.d/vscodium.gpg
-  
+
   # Add the repository
   echo \
     'deb https://paulcarroty.gitlab.io/vscodium-deb-rpm-repo/debs/ vscodium main' \
     | sudo tee --append /etc/apt/sources.list.d/vscodium.list
-  
+
   # Update and install
   sudo apt-get update -y
   sudo apt-get install -y codium
