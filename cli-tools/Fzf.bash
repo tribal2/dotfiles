719570d73427b7994989533dc36e8a2f38bdb41f
Ricardo Tribaldos <579671+swordf1zh@users.noreply.github.com>
2020-12-29 00:16:28 -0500

    feat: add Forgit installation

--- cli-tools/Fzf.bash
+++ cli-tools/Fzf.bash
@@ -8,3 +8,15 @@ else
 
   ln -sf "$BASEDIR"/bash_/fzf.bash "$BASHDIR"/fzf.bash
 fi
+
+# https://github.com/wfxr/forgit.git
+if [[ ! -d "$SRCDIR"/forgit ]]
+then
+  read -p 'Do you want to install Forgit (a utility tool powered by fzf for using git interactively)? (y/n) ' -n 1 -r
+  echo
+  if [[ $REPLY =~ ^[Yy]$ ]]
+  then
+    git clone https://github.com/wfxr/forgit.git "$SRCDIR"/forgit
+    ln -sf "$SRCDIR"/forgit/forgit.plugin.sh "$BASHDIR"/forgit.bash
+  fi
+fi
