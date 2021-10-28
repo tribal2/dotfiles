714fcadc7b992d99b179219abdef62e9b77249dc
Ricardo Tribaldos <579671+tribal2@users.noreply.github.com>
2021-10-28 14:32:02 -0500

    refactor: modify Sherlock alias and run with Docker

--- bash_/sherlock.bash
+++ bash_/sherlock.bash
@@ -1,7 +1,6 @@
-alias sherlock='python3 \
-  $HOME/src/sherlock/sherlock \
-  --site facebook \
-  --site instagram \
-  --site twitter \
-  --site youtube \
-  --output /dev/null'
+alias sherlock='docker run --rm \
+  theyahya/sherlock \
+    --site facebook \
+    --site instagram \
+    --site twitter \
+    --site youtube'
--- cli-tools/Sherlock.bash
+++ cli-tools/Sherlock.bash
@@ -1,16 +1,8 @@
 # https://github.com/sherlock-project/sherlock
 
-if [[ -d "$SRCDIR"/sherlock ]]
+if [[ -f "$BASHDIR"/sherlock.bash ]]
 then
   echo "Already installed!"
 else
-  git clone https://github.com/sherlock-project/sherlock.git "$SRCDIR"/sherlock
-  
-  [[ $(python3 -m pip --version) ]] || sudo apt-get install -y python3-pip
-
-  cd "$SRCDIR"/sherlock || exit
-  python3 -m pip install -r requirements.txt
-  cd "$HOME" || exit
-
   ln -sf "$BASEDIR"/bash_/sherlock.bash "$BASHDIR"/sherlock.bash
 fi
