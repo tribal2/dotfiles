537267ea36f2589efe4c67e6a96fe8ca4363a117
Ricardo Tribaldos <579671+tribal2@users.noreply.github.com>
2021-11-24 15:12:20 -0500

    feat: ask user to include Docker aliases in Bash-only setup

--- setup_bash_only.sh
+++ setup_bash_only.sh
@@ -8,12 +8,25 @@ mv .bashrc{,$BACKUP_TAG}
 mv .bash_aliases{,$BACKUP_TAG}
 
 echo 'Downloading Bash config files from tribal2/dotfiles...'
+
 curl https://raw.githubusercontent.com/tribal2/dotfiles/master/.bashrc \
   --silent \
   --output .bashrc
+
 curl https://raw.githubusercontent.com/tribal2/dotfiles/master/bash_/aliases.bash \
   --silent \
   --output .bash_aliases
+
+echo
+read -p 'Do you want to include Docker aliases? (y/n) ' -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  echo '#-------- Bash aliases for Docker --------' >> .bash_aliases
+  curl https://raw.githubusercontent.com/tribal2/dotfiles/master/bash_/docker.bash \
+    --silent \
+  >> .bash_aliases
+fi
 
 echo 'Sourcing new Bash files...'
 source .bashrc
