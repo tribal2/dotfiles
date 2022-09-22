139ad0174a78edbe0ca77b453e547ebe328e1eb2
Ricardo Tribaldos <579671+tribal2@users.noreply.github.com>
2022-09-22 10:27:11 -0500

    feat: add alias for gitk

--- cli-tools/Gitk.bash
+++ cli-tools/Gitk.bash
@@ -3,4 +3,7 @@ then
   echo "Already installed!"
 else
   sudo apt-get install -y gitk
+
+  # Add Git alias: git visual -> Execute gitk --all in a background process
+  git config --global alias.visual '!gitk --all &'
 fi
